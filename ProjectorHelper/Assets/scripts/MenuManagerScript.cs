d63f3e5acc9e0076f892739fa5fe239be3427d44
Peter Chow <peterchow@MAIN.IMSE.HKU.HK>
2016-12-08 10:39:32 +0800

    add menu manager

---
+++ ProjectorHelper/Assets/scripts/MenuManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuManagerScript : MonoBehaviour {
+
+    public List<GameObject> menus;
+    public int defaultMenuIndex;
+
+	// Use this for initialization
+	void Start () {
+        GoToMenu(defaultMenuIndex);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SetMenusActive (bool isActive) {
+        foreach (GameObject m in menus) {
+            m.SetActive(isActive);
+        }
+    }
+
+    public void GoToMenu (int index) {
+        SetMenusActive(false);
+        menus[index].SetActive(true);
+    }
+}
